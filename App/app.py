67d2ee318b170e152e6a08e29547e08f7acd3638
Aashish-Charaya <aashish.charaya@gmail.com>
2023-10-29 10:58:19 +0530

    font change

--- App/app.py
+++ App/app.py
@@ -7,20 +7,12 @@ st.set_page_config(page_title="My Streamlit App")
 
 
 
-st.markdown(
-    """
-    <style>
-        .theory, .text {
-            font-size: 25px !important;
-            text-align: justify;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+
 
 def main():
-    menu = ["Home", "The Covid Effect", "Top 10 Startups Revenue generation wise", "Page 3"]
+
+    menu = ["Home", "The Covid Effect", "Top 10 Startups Revenue generation wise", "The Affected Sectors"]
     choice = st.sidebar.selectbox("Select a page", menu)
 
     if choice == "Home":
@@ -35,34 +27,37 @@ def main():
             " contribute to a better understanding of the Indian startup ecosystem's growth in the AI and EV sectors, providing"
             " valuable insights for entrepreneurs, investors, policymakers, and other stakeholders. You can use openly available"
             " news and/or any publicly available datasets for performing this case study."
-            "</div>",
+            "</div>"
+            
+            
+            
+            ,
             unsafe_allow_html=True
         )
 
         st.header("Key Questions to Explore:")
-
         st.markdown(
-        """
-        <Div class='theory'>
-        <ul>
-            <li>What are the top 10 startups in terms of revenue over the last 5 years?</li>
-            <li>Give a location-wise distribution of the top 100 startups.</li>
-            <li>Give a comparison between Foreign and domestic funds and the sectors they are investing in.</li>
-            <li>Valuation based analysis on Fintech companies over the last 5 years.</li>
-            <li>How did Covid affect funding for startups across different sectors?</li>
-            <li>Which sectors have had the most number of new and emerging startups?</li>
-            <li>Compare the growth rate of startups with their age.</li>
-            <li>How do funding rounds affect the growth of startups?</li>
-            <li>How has funding for different sectors, like EV evolved over time?</li>
-            <li>Startups in which sectors have been stagnating or decreasing.</li>
-        </ul>
-        </Div>
-        """,
-        unsafe_allow_html=True
+            "<div class='text'>"
+            "<ul>"
+            "<li>What are the top 10 startups in terms of revenue over the last 5 years?</li>"
+            "<li>Give a location-wise distribution of the top 100 startups.</li>"
+            "<li>Give a comparison between Foreign and domestic funds and the sectors they are investing in.</li>"
+            "<li>Valuation based analysis on Fintech companies over the last 5 years.</li>"
+            "<li>How did Covid affect funding for startups across different sectors?</li>"
+            "<li>Which sectors have had the most number of new and emerging startups?</li>"
+            "<li>Compare the growth rate of startups with their age.</li>"
+            "<li>How do funding rounds affect the growth of startups?</li>"
+            "<li>How has funding for different sectors, like EV evolved over time?</li>"
+            "<li>Startups in which sectors have been stagnating or decreasing.</li>"
+            "</ul>"
+            "</div>",
+            unsafe_allow_html=True
         )
 
-        st.write(
-            "To explore these questions, navigate through the pages using the sidebar on the left."
+
+        st.markdown(
+            "<h4>To explore these questions, navigate through the pages using the sidebar on the left.</h4>",
+            unsafe_allow_html=True
         )
 
     elif choice == "The Covid Effect":
@@ -77,9 +72,25 @@ def main():
         st.write("## Revenue Growth Over the Years")
         st.pyplot(create_line_chart(df))
 
-    elif choice == "Page 3":
-        st.title("Page 3")
-        st.write("This is page 3.")
+    elif choice == "The Affected Sectors":
+        
+        st.header("Startups in which sectors have been stagnating or decreasing.")
+        st.markdown("""
+        <ol class='big-font'>
+            <li>Online Travel Agencies: The COVID-19 pandemic had a significant impact on travel-related startups in India. The restrictions on travel and reduced tourism affected the growth of online travel agencies.</li>
+            <li>Hyperlocal Delivery Services: Hyperlocal delivery startups, particularly those focused on delivering groceries, faced stiff competition and margin pressures. Some experienced stagnation due to challenges in achieving profitability.</li>
+            <li>FoodTech: The food delivery sector in India was highly competitive, with many startups struggling to achieve profitability. The market was saturated, and customer acquisition costs were high.</li>
+            <li>Real Estate Tech: The real estate technology sector in India faced challenges due to market volatility and regulatory complexities. Startups in this space often found it difficult to scale.</li>
+            <li>E-commerce: While e-commerce remained a significant sector, intense competition, and the dominance of major players made it challenging for newer startups to gain a foothold.</li>
+            <li>Education Technology (EdTech): The EdTech sector was highly competitive, and some segments of the market were saturated. Regulatory changes also posed challenges for certain types of educational startups.</li>
+            <li>Renewable Energy: Renewable energy startups faced challenges related to funding and regulatory complexities. The sector required substantial capital investment, which was not always readily available.</li>
+            <li>Agriculture Tech: While agriculture technology had potential, adoption in traditional farming communities and complex supply chain issues were obstacles for startups in this sector.</li>
+            <li>Fashion and Apparel: Fashion e-commerce startups faced challenges due to changing consumer preferences, high return rates, and the need for constant inventory updates.</li>
+            <li>Gaming: The mobile gaming sector, while growing, was highly competitive. Monetization and user acquisition challenges led to stagnation for some gaming startups.</li>
+            <li>Healthcare: While healthcare startups were growing, the sector was highly regulated, and startups faced challenges related to funding and scaling.</li>
+        </ol>""",
+        unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()
